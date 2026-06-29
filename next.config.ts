cfff746dd7d00fe9fab9c055a58355e49f83e280
Tiago Miguel Gonçalves Costa <tiagocosta18@mac.home>
2026-06-29 18:09:52 +0100

    fix: whitelist static2.finnhub.io for next/image

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static2.finnhub.io',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
